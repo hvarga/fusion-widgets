d4fbd17d4c4148b3a9720d810c822f22a76d15a2
hvarga <hrvoje.varga@gmail.com>
2014-03-13 22:31:17 +0100

    Implemented reading map selection properties with the Fusion API.
    Change-Id: I85d184413547f46e87925e7e4e4e510d83c1c633

--- fusion-widgets/widgets/SelectionReport/src/js/SelectionReport.js
+++ fusion-widgets/widgets/SelectionReport/src/js/SelectionReport.js
@@ -12,6 +12,8 @@ Fusion.Widget.SelectionReport = OpenLayers.Class(Fusion.Widget, {
      */
     uiClass: Jx.Button,
 
+    map: null,
+
     /**
      * Method is called automatically when the widget needs to be initialized by the platform.
      * When called, it will initialize and enable the SelectionReport widget.
@@ -32,5 +34,56 @@ Fusion.Widget.SelectionReport = OpenLayers.Class(Fusion.Widget, {
      */
     activate: function() {
         // TODO: Implement the generating of report.
+
+        // Get the reference to the widget responsible for rendering of the map.
+        map = Fusion.getWidgetById("Map");
+        // Now that we have a referece to the map widget, we need to get the selected parcels.
+        // This is acomplished using the getSelection() of the Fusion.Widget object.
+        // The getSelection() method is an asynchronous call so we need to register a callback method.
+        // The callback method will be called when getSelection() finishes its processing.
+        map.getSelection(this.displaySelection);
+    },
+
+    /**
+     * This is a callback method which will be called when the map widget finishes the processing of the getSelection()
+     * method. This will give us a selection object which will contain all the selected parcels from the map.
+     *
+     * @method displaySelection
+     * @param selection Object which will contain all the selected parcels from the map widget.
+     */
+    displaySelection: function(selection) {
+        // We need to read the map name from the selection object.
+        // There can be actually multiple maps in one selection object. Which is kind of weird, since the map
+        // widget itself can only render one map at a time. So, I will only take the first map.
+        // The rest (if this is possible) will be ignored.
+        var mapSelection = null;
+        for (var mapName in selection) {
+            mapSelection = selection[mapName];
+            break;
+        }
+
+        // Since map can contain selections from the multiple layers, we need to iterate over each layer.
+        var numberOfLayers = mapSelection.getNumLayers();
+        for (var layerNum = 0; layerNum < numberOfLayers; layerNum++) {
+            var layer = mapSelection.getLayer(layerNum);
+
+            // Layer can also have multiple features and we need to iterate over each one.
+            var numberOfSelectedElements = layer.getNumElements();
+            var layerPropertyNames = layer.getPropertyNames();
+            var numberOfLayerProperties = layer.getNumProperties();
+            if (numberOfSelectedElements > 0) {
+                // Get each selected element.
+                for (var elementNumber = 0; elementNumber < numberOfSelectedElements; elementNumber++) {
+                    // Now that we have an element, read its properties. Element can have multiple properties
+                    // so we need to iterate over each one and read its value.
+                    for (var propertyNum = 0; propertyNum < numberOfLayerProperties; propertyNum++) {
+                        var propertyName = layerPropertyNames[propertyNum];
+                        var propertyValue = layer.getElementValue(elementNumber, propertyNum);
+
+                        console.log("propertyName: " + propertyName + "; propertyValue: " + propertyValue);
+                    }
+                }
+            }
+        }
     }
 });
